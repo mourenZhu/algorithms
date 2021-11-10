3948bb3c79332878f46e53bf6b3580b22eba7586
mourenZhu <1207743591@qq.com>
2021-11-10 22:48:48 +0800

    增加希尔排序代码描述

--- src/main/java/algorithms/sort/Shell.java
+++ src/main/java/algorithms/sort/Shell.java
@@ -17,7 +17,9 @@ public class Shell extends Sort {
             h = 3 * h + 1;
         }
         while (h >= 1) {
+            // 开启相距h的列表，每一次新的循环就是给一个新的值来开启插入排序，其中每h次循环后就是开启一个新的列表
             for (int i = h; i < l; i++) {
+                // 这里就是插入排序的精髓，插入时排序，保证下次插入时前面的数都是有序的
                 for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
                     exch(a, j, j - h);
                 }
