665e0cbb79655a51b8720bd3ec2a39b0b9b29d98
mourenZhu <1207743591@qq.com>
2021-11-15 12:31:36 +0800

    分离测试代码

---
+++ src/test/java/cn/zhumouren/algorithms/MainTest.java
@@ -0,0 +1,14 @@
+package cn.zhumouren.algorithms;
+
+/**
+ * @author mourenZhu
+ * @version 1.0
+ * @description 测试类
+ * @date 2021/11/15 9:36
+ **/
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Test!");
+    }
+
+}
